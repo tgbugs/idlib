51f332818f780346c52b3d9360a7664d36a4af5b
Tom Gillespie <tgbugs@gmail.com>
2019-11-25 17:12:05 -0800

    Identifier.asLocal and implementation notes

--- idlib/core.py
+++ idlib/core.py
@@ -30,6 +30,24 @@ class QNames(LocalConventions):
 
 class Identifier(str):  # TODO decide if str should be base ...
     """ Base class for all Identifiers """
+    # all identifiers mapped to idlib should have a string representation
+    # or be representable as strings in as unambiguous a way as possible
+    # this means that distinctions based on types e.g. MyId('1') and YourId('1')
+    # need stringify in such a way that they do not colide e.g. myid:1 yourid:1
+    # within the expected context of their expansion
+    # local:?:myid:1 local:?:yourid:1 could represent the space of local identifiers
+    # with unknown local conventions, maintaining a record of all local conventions
+    # seems likely to be a giant pain, so local:?: ids would be ephemoral and would
+    # probably have to be marked with a source as a kind of best guess maximal domain
+    # for assuming similarity, though in some documents every instance of a local:?:id
+    # should probably be assumed to be different under expansion
+
+    # as a result of this, it is still not entirely clear whether
+    # str is quite the right option, but since normalization can
+    # occur before stringification, it is probably ok ...
+    # e.g. hello:world -> local:?:hello:world in cases where
+    # a local identifier is used without conventions
+    # same with local:?:1, local:?:2, local:?:3, local:?:4
 
     @staticmethod
     def normalize(identifier):
@@ -47,6 +65,12 @@ class Identifier(str):  # TODO decide if str should be base ...
 
     def data(self):
         raise NotImplementedError
+
+    def asLocal(self, conventions=None):
+        if conventions is None:
+            conventions = self._local_conventions
+
+        return conventions(self)
 
 
 # IETF
